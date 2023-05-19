478f4648746d871d604eec2ababd71bc3ba9e6d5
hernan arica <hernanaricammm@gmail.com>
2023-05-19 12:37:44 -0300

    se cambian el metodo login de get a post

--- src/components/ButtonLogin.tsx
+++ src/components/ButtonLogin.tsx
@@ -5,7 +5,7 @@ export function ButtonLogin() {
   const login = useGoogleLogin({
     onSuccess: async tokenResponse => {
       const accessToken = tokenResponse.access_token;
-      const res = await axios.get(`http://localhost/api/auth/google/callback/${ accessToken }`);
+      const res = await axios.post(`http://localhost/api/auth/login`, { accessToken });
 
       console.log(res);
     },
